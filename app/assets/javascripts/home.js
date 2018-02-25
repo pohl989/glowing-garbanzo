31677484713d15aaa9512f4e12a149bdd545e74f
pohl989 <pohl989@gmail.comm>
2018-02-25 14:36:06 -0700

    remove extras

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,15 +1,17 @@
 console.log("home coffee woot woot");
 
-$('.modal').modal({
-    dismissible: true, // Modal can be dismissed by clicking outside of the modal
-    opacity: .5, // Opacity of modal background
-    inDuration: 300, // Transition in duration
-    outDuration: 200, // Transition out duration
-    startingTop: '4%', // Starting top style attribute
-    endingTop: '10%', // Ending top style attribute
-    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-      console.log(modal, trigger);
-    },
-    // complete: function() { alert('Closed'); } // Callback for Modal close
-  }
-);
+$( document ).ready(function(){
+    $('.modal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        },
+        // complete: function() { alert('Closed'); } // Callback for Modal close
+      }
+    );
+    $(".button-collapse").sideNav();
+});
