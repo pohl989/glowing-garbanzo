bb086937b2f964dca1eb9031f0d3bcdaee1760d8
pohl989 <pohl989@gmail.comm>
2018-02-25 19:02:43 -0700

    moved to postgres

--- Gemfile
+++ Gemfile
@@ -9,7 +9,8 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0.rc1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
+gem 'pg', '~> 0.21.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224180154) do
+ActiveRecord::Schema.define(version: 2018_02_24_180154) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
