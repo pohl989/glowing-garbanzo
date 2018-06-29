581837253d1f01cd5cd663e8304c4ff4fe3e7c4f
Ben Pohl <pohl989@gmail.com>
2018-06-28 20:49:01 -0600

    add new routes and contact model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
 class HomeController < ApplicationController
+
   def index
-  end 
+
+  end
+
+  def portfolio
+  end
+
+  def background
+  end
+
+  def bio
+  end
+
+  def course_goals
+  end
+
+  def about
+    @contact = Contact.new
+  end
+
+  def create
+  end
+
+
+
+  private
+
+
+
 end
