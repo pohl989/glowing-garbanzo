53661785d4a84361009dce5371c994619e69873a
pohl989 <pohl989@gmail.com>
2018-10-16 19:06:42 -0600

    removed a bunch of extras

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts
-  devise_for :models
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
   get 'portfolio', :to => 'home#portfolio'
@@ -9,7 +6,5 @@ Rails.application.routes.draw do
   get 'bio', :to => 'home#bio'
   get 'course_goals', :to => 'home#course_goals'
   get 'about', :to => 'home#about'
-
-  resources :contacts
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_021903) do
+ActiveRecord::Schema.define(version: 2018_08_28_003609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2018_06_29_021903) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
